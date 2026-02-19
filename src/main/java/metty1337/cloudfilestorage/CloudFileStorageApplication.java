1060f34a9fded1020e291e1d13c2ae7994cc4e9d
Metty1337 <c137morty3@gmail.com>
2026-02-19 19:52:17 +0300

    chore: init

---
+++ src/main/java/metty1337/cloudfilestorage/CloudFileStorageApplication.java
@@ -0,0 +1,13 @@
+package metty1337.cloudfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFileStorageApplication.class, args);
+    }
+
+}
