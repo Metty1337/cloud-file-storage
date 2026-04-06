324c472d3794e13e9b8a0343c8c20e402f64bb57
Metty1337 <c137morty3@gmail.com>
2026-04-06 19:09:54 +0300

    refactor(UserService):remove @Transactional from interface

--- backend/src/main/java/metty1337/cloudfilestorage/service/UserService.java
+++ backend/src/main/java/metty1337/cloudfilestorage/service/UserService.java
@@ -3,14 +3,11 @@ package metty1337.cloudfilestorage.service;
 import metty1337.cloudfilestorage.dto.request.SignUpRequest;
 import metty1337.cloudfilestorage.dto.response.SignUpResponse;
 import metty1337.cloudfilestorage.entity.User;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserService {
-    @Transactional
     SignUpResponse createUser(SignUpRequest signUpRequest);
 
-    @Transactional(readOnly = true)
     Optional<User> findByUsername(String username);
 }
