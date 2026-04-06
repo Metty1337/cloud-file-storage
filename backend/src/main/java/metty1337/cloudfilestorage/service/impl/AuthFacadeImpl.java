65a96469076398f67b49f0925dbfb0cb99298f33
Metty1337 <c137morty3@gmail.com>
2026-04-06 17:23:15 +0300

    refactor: clean unused imports, fix wrong name

--- backend/src/main/java/metty1337/cloudfilestorage/service/impl/AuthFacadeImpl.java
+++ backend/src/main/java/metty1337/cloudfilestorage/service/impl/AuthFacadeImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class AuthFacadeImlp implements AuthFacade {
+public class AuthFacadeImpl implements AuthFacade {
 
     private final AuthenticationManager authenticationManager;
     private final HttpSessionSecurityContextRepository securityContextRepository;
