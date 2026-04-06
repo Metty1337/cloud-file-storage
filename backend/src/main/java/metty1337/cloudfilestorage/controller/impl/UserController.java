ba971a95acd6288d65194bade3d19d39c9f4dcef
Metty1337 <c137morty3@gmail.com>
2026-04-06 18:39:03 +0300

    refactor: replace manual creating response with mapper

--- backend/src/main/java/metty1337/cloudfilestorage/controller/impl/UserController.java
+++ backend/src/main/java/metty1337/cloudfilestorage/controller/impl/UserController.java
@@ -1,8 +1,10 @@
 package metty1337.cloudfilestorage.controller.impl;
 
 import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
 import metty1337.cloudfilestorage.controller.UserControllerApi;
 import metty1337.cloudfilestorage.dto.response.UserResponse;
+import metty1337.cloudfilestorage.mapper.UserMapper;
 import metty1337.cloudfilestorage.security.UserPrincipal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class UserController implements UserControllerApi {
+
+    private final UserMapper userMapper;
 
     @GetMapping("/me")
     @Override
     public ResponseEntity<UserResponse> me(@Parameter(hidden = true) @AuthenticationPrincipal UserPrincipal userPrincipal) {
-        UserResponse response = new UserResponse(userPrincipal.getUsername());
+        UserResponse response = userMapper.toUserResponse(userPrincipal);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- backend/src/main/java/metty1337/cloudfilestorage/mapper/UserMapper.java
+++ backend/src/main/java/metty1337/cloudfilestorage/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package metty1337.cloudfilestorage.mapper;
 import metty1337.cloudfilestorage.dto.request.SignUpRequest;
 import metty1337.cloudfilestorage.dto.response.SignInResponse;
 import metty1337.cloudfilestorage.dto.response.SignUpResponse;
+import metty1337.cloudfilestorage.dto.response.UserResponse;
 import metty1337.cloudfilestorage.entity.User;
 import metty1337.cloudfilestorage.security.UserPrincipal;
 import org.mapstruct.Mapper;
@@ -19,4 +20,6 @@ public interface UserMapper {
     SignUpResponse toSignUpResponse(User user);
 
     SignInResponse toSignInResponse(UserPrincipal userPrincipal);
+
+    UserResponse toUserResponse(UserPrincipal userPrincipal);
 }
